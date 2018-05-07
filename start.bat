a1c247d902ad3f987295295c9574f40b7bd8a50f
Unknown <tukigase@gmail.com>
2018-05-07 22:12:36 +0900

    Use Python36 environment.

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-call C:\Users\user\Anaconda3\Scripts\activate.bat C:\Users\user\Anaconda3\envs\Standard-Py27
+call C:\Users\user\Anaconda3\Scripts\activate.bat C:\Users\user\Anaconda3\envs\Standard-Py36
 edic2epwing.py out
